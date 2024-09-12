5d228946a96d1e32c1b476eedba2e376badda99d
CYberGnosticism <127010679+SWORDi0@users.noreply.github.com>
2024-09-12 11:37:22 +0500

    Add files via upload
    michurin ilya 2

--- main.py
+++ main.py
@@ -1,67 +1,49 @@
-
 import math
-#№18
-def pip (byte, pages, strokes, simbols):
-    peremnozj = pages * strokes * simbols
-    bit = byte * 8
-    N = bit/peremnozj
-    log = 2 ** N
-    print(str(log) + " Количество букв в алфавите")
+def int_fract(dec_num):
+    int_part = "" #целая часть
+    fract_part = "" #дробная часть
+    flag = False
+    for i in dec_num:
+        if i ==",":
+            flag = True
+            continue
+        if flag == False:
+            int_part = int_part + i
+        else:
+            fract_part = fract_part + i
+    return str(int_conv(int_part)) + "," + str(fract_conv(fract_part))
 
+def int_conv(int_part):
+    lenOfint_part = len(int_part)
+    sum = 0
+    power = 0
+    cor = 1
+    result = 0
+    while power < lenOfint_part:
+        number = int(int_part[power])
+        sum = number * (2**(lenOfint_part - cor))
+        power +=1
+        cor +=1
+        result = result + sum
+    return result
 
-#№19
-def ziz (powerz1,powerz2):
-    alfm1 = math.log2(powerz1)
-    alfm2 = math.log2(powerz2)
-    diff = alfm2/alfm1
-    print(str(diff)+ " разница между алфавитами")
-
-#№20
-def jij (simbols, bit):
-    logz = math.log2(simbols)
-    otvet = bit/logz
-    print(str(otvet) + " количество символов в сообщении")
-    inp7 = input("количество символов в алфавите: ")
-    inp8 = input("количество бит: ")
-    jij(int(inp7), int(inp8))
-#№19
-def vivo (bit):
-    tat = 2 ** bit
-    print(str(tat) + " Количество этажей в доме")
-
-#№20
-def vpivo (bit):
-    tat1 = 2 ** bit
-    print(str(tat1) + " Количество подъездов в доме")
-
-
-#№7
-def task(i, K):
-    N = 2**i * K
-    WB = 8 + 8
-    Brown = N - WB
-    return Brown
-
+def fract_conv(fract_part):
+    lenOffract_part = len(fract_part)
+    sum = 0
+    power = 0
+    cor = -1
+    result = 0
+    while power < lenOffract_part:
+        number = int(fract_part[power])
+        # print(number)
+        sum = number * (2**(cor))
+        # print(sum)
+        power +=1
+        cor -=1
+        result = result + sum
+    result = str(result)
+    return result[2:]
 
 if __name__ == "__main__":
-    inp1 = input("количество байт: ")
-    inp2 = input("количество cтраниц: ")
-    inp3 = input("количество строк: ")
-    inp4 = input("количество символов: ")
-    pip(int(inp1), int(inp2), int(inp3), int(inp4))
-    inp5 = input("мощность алфавита№1: ")
-    inp6 = input("мощность алфавита№2: ")
-    ziz(int(inp5), int(inp6))
-    inp9 = input("информации об этаже: ")
-    vivo(int(inp9))
-    inp10 = input("информации о подъезде: ")
-    vpivo(int(inp10))
-    inp11 = int(input("Введите вес 1 банки белой краски: "))
-    inp12 = int(input("Введите количество банок синей краски: "))
-    print("Количество банок коричневой краски:  ", task(inp11, inp12))
-
-
-
-
-
-
+    dec_num = str(input("введите целое число: "))
+    print(int_fract(dec_num))
